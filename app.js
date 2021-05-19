58f324da94cfa663ca7ac0e663150fa0ba01cb93
Ravi <ravisinghxxxxl@gmail.com>
2021-05-19 14:11:28 +0300

    v0.1

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('start');
+console.log("start");
+
+var screen = document.getElementById("screen");
+var button = document.querySelectorAll("button");
+
+let screenValue = '';
+
+for (items of button) {
+  items.addEventListener("click", e => {
+    buttonText = e.target.innerText;
+
+    if (buttonText == 'C') {
+        screenValue = "";
+        screen.value = screenValue;
+    }
+    else if (buttonText == '=') {
+        screen.value = eval(screenValue);
+    }
+    else {
+        screenValue += buttonText;
+        screen.value = screenValue; 
+    }
+  });
+}
